4e1f8740356f0e8b2291d128ffa550b31a7315f3
malalwan <51250170+malalwan@users.noreply.github.com>
2019-06-10 15:41:56 +0530

    cohort 4 page

---
+++ cohort4.php
@@ -0,0 +1,20 @@
+<?php 
+  $siteRoot='/dashboard';
+  $index=4;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>KPI Dashboard</title>
+<link href="styles.css" rel="stylesheet" type="text/css">
+</head>
+
+<?php require './includes/header.php'?>
+<h2 align="center">Cohort 4</h2>
+
+<!-- Page content -->
+<div class="main">
+
+</div>
+</html>
