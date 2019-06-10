c15c39886532a255dca31957a25bc015efc72580
malalwan <51250170+malalwan@users.noreply.github.com>
2019-06-10 15:43:25 +0530

    includes folder added

---
+++ includes/header.php
@@ -0,0 +1,13 @@
+<h1 align="center">Cisco Launchpad : Startup Analysis</h1>
+
+<hr>
+<div class="sidenav">
+  <img src='./images/logo_wide.jpg' alt="logo" style="width:160px;height:80px;" align="center">
+  <a href="<?= $siteRoot; ?>/overalls.php">Overalls</a>
+  <a href="<?= $siteRoot; ?>/cohort1.php">Cohort 1</a>
+  <a href="<?= $siteRoot; ?>/cohort2.php">Cohort 2</a>
+  <a href="<?= $siteRoot; ?>/cohort3.php">Cohort 3</a>
+  <a href="<?= $siteRoot; ?>/cohort4.php">Cohort 4</a>
+  <a href="<?= $siteRoot; ?>/cohort5.php">Cohort 5</a>
+  <a href="<?= $siteRoot; ?>/about.php">About</a>
+</div>
