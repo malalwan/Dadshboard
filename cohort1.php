592d360a1036554d7fccfe967e1638a560a9e50a
malalwan <51250170+malalwan@users.noreply.github.com>
2019-06-10 15:40:03 +0530

    cohort 1 base page

---
+++ cohort1.php
@@ -0,0 +1,20 @@
+<?php 
+  $siteRoot='/dashboard';
+  $index=1;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>KPI Dashboard</title>
+<link href="styles.css" rel="stylesheet" type="text/css">
+</head>
+
+<?php require './includes/header.php'?>
+<h2 align="center">Cohort 1</h2>
+
+<!-- Page content -->
+<div class="main">
+
+</div>
+</html>
